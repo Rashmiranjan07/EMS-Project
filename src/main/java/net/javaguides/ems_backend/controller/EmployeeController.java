82642f52afcfaababeeb9a4faba3b5c017892ac3
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-08 23:07:02 +0530

    EMS backend

--- src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
+++ src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,10 +48,16 @@ public class EmployeeController {
 	// build Update Employee REST API
 	@PutMapping("{id}")
 	public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId,
-		@RequestBody EmployeeDto updatedEmployee) {
+			@RequestBody EmployeeDto updatedEmployee) {
 		EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
 		return ResponseEntity.ok(employeeDto);
+	}
 
+	// build Delete Employee REST API
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
+		employeeService.deleteEmployee(employeeId);
+		return ResponseEntity.ok("Employee deleted successfully");
 	}
 
 }
