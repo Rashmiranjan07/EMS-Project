378da3d2e55beab6b9c32e274bb83fd876970862
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-08 19:46:19 +0530

    ems backend

--- src/main/java/net/javaguides/ems_backend/service/EmployeeService.java
+++ src/main/java/net/javaguides/ems_backend/service/EmployeeService.java
@@ -4,5 +4,7 @@ import net.javaguides.ems_backend.dto.EmployeeDto;
 
 public interface EmployeeService {
 	EmployeeDto createEmployee(EmployeeDto employeeDto);
+	
+	EmployeeDto getEmployeeById(Long employeeId);
 
 }
