771a2404a49de53d54c6ebfc1ed945db4011c7d5
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-11 21:46:10 +0530

    Added Frontend

--- frontend/src/components/ListEmployeeComponent.jsx
+++ frontend/src/components/ListEmployeeComponent.jsx
@@ -1,44 +1,49 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react';
+import { listEmployees } from '../services/EmployeeService';
 
 const ListEmployeeComponent = () => {
 
-    const [employees, setEmployees] = useState([])
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        listEmployees()
+            .then((response) => {
+                setEmployees(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
 
     return (
-        <div className='container'>
+        <div className="container">
 
-            <h2 className='text-center'>List of Employees</h2>
-            <table className='table table-striped table-bordered'>
+            <h2 className="text-center">List of Employees</h2>
+
+            <table className="table table-striped table-bordered">
                 <thead>
                     <tr>
                         <th>Employee Id</th>
                         <th>Employee First Name</th>
                         <th>Employee Last Name</th>
                         <th>Employee Email Id</th>
-
                     </tr>
                 </thead>
+
                 <tbody>
-
-                    {
-                        dummyData.map(employee =>
-                            <tr key={employee.id}>
-                                <td>{employee.id}</td>
-                                <td>{employee.firstName}</td>
-                                <td>{employee.lastName}</td>
-                                <td>{employee.email}</td>
-
-                            </tr>
-                        )
-                    }
-
+                    {employees.map((employee) => (
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.email}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
 
-
         </div>
-    )
-}
+    );
+};
 
-export default ListEmployeeComponent
+export default ListEmployeeComponent;
