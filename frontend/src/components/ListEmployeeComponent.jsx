bec9c55a70ae03e0c915153781195e1472282393
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-11 21:18:27 +0530

    added frontend

--- frontend/src/components/ListEmployeeComponent.jsx
+++ frontend/src/components/ListEmployeeComponent.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 
-function ListEmployeeComponent() {
-  return (
-    <div>ListEmployeeComponent</div>
-  )
+const ListEmployeeComponent = () => {
+
+    const dummyData = [
+        {
+            "id": 1,
+            "firstName": "Rashmiranjan",
+            "lastName": "Shaw",
+            "email": "heyyrashmiranjan02@gmail.com"
+        },
+        {
+            "id": 2,
+            "firstName": "Rashmiranjan",
+            "lastName": "Shaw",
+            "email": "heyyrashmiranjan02@gmail.com"
+        },
+        {
+            "id": 3,
+            "firstName": "Rashmiranjan",
+            "lastName": "Shaw",
+            "email": "heyyrashmiranjan02@gmail.com"
+        },
+        {
+            "id": 4,
+            "firstName": "Rashmiranjan",
+            "lastName": "Shaw",
+            "email": "heyyrashmiranjan02@gmail.com"
+        }
+    ]
+
+
+
+    return (
+        <div className='container'>
+
+            <h2 className='text-center'>List of Employees</h2>
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Employee Id</th>
+                        <th>Employee First Name</th>
+                        <th>Employee Last Name</th>
+                        <th>Employee Email Id</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        dummyData.map(employee =>
+                            <tr key={employee.id}>
+                                <td>{employee.id}</td>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.email}</td>
+
+                            </tr>
+                        )
+                    }
+
+                </tbody>
+            </table>
+
+
+        </div>
+    )
 }
 
 export default ListEmployeeComponent
