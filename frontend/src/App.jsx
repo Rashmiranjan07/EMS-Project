7adc476529bcf0774cf88b55b7d97529553d1b33
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-13 20:00:24 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
