4303c8039b0e51334d4e0543355bdb0280b4b1fc
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-13 20:01:34 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,13 @@ import { BrowserRouter } from 'react-router-dom'
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent  />
+      <BrowserRouter>
+
+        <HeaderComponent />
+        <ListEmployeeComponent />
+        <FooterComponent />
+
+      </BrowserRouter>
     </div>
   )
 }
