2fae77e181ab2d47650710ff5a96f68b0dd38e35
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-14 20:26:15 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <Route path='/' element={ <ListEmployeeComponent />}></Route>
           {/* // http://localhost:3000/employees */}
           <Route path='/employees' element={ <ListEmployeeComponent />}></Route>
-
+          {/* // http://localhost:3000/add-employee */}
           <Route path='/add-employee' element={<EmployeeComponent />}></Route>
 
         </Routes>
