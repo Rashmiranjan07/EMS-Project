f9ddd7bb2fb0c15ee4549701beaf4e05e1681485
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-18 11:48:27 +0530

    frontend

--- frontend/src/components/EmployeeComponent.jsx
+++ frontend/src/components/EmployeeComponent.jsx
@@ -27,7 +27,7 @@ const EmployeeComponent = () => {
     function saveEmployee(e) {
         e.preventDefault();
 
-        if (validateForm) {
+        if (validateForm()) {
             const employee = { firstName, lastName, email }
             console.log(employee)
 
