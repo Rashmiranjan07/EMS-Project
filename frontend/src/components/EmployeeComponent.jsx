ef4b68075d9f2cc7002dbb2d15b96366cdb1a708
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-18 11:27:29 +0530

    frontend

--- frontend/src/components/EmployeeComponent.jsx
+++ frontend/src/components/EmployeeComponent.jsx
@@ -10,7 +10,7 @@ const EmployeeComponent = () => {
     const [email, setEmail] = useState('')
 
 
-    useState({
+    const [errors, setErrors]=useState({
         firstName:'',
         lastName:'',
         email:''
