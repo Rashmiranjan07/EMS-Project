453615e5e6f5cb4fed0bba2b48f4f28f37f599f5
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-18 12:35:06 +0530

    frontend

--- frontend/src/components/EmployeeComponent.jsx
+++ frontend/src/components/EmployeeComponent.jsx
@@ -1,6 +1,6 @@
 // import { create } from 'axios'
 import React, { useEffect, useState } from 'react'
-import { createEmployee, getEmployee } from '../services/EmployeeService'
+import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeService'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
@@ -37,22 +37,30 @@ const EmployeeComponent = () => {
         }
     }, [id])
 
-    function saveEmployee(e) {
+    function saveOrUpdateEmployee(e) {
         e.preventDefault();
 
         if (validateForm()) {
             const employee = { firstName, lastName, email }
             console.log(employee)
 
-            createEmployee(employee).then((response) => {
-                console.log(response.data);
-                navigator('/employees')
-            })
-
+            if (id) {
+                updateEmployee(id, employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees');
+                }).catch(error => {
+                    console.error(error);
+                })
+            } else {
+                createEmployee(employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.error(error);
+                })
+            }
         }
-
     }
-
 
 
     function validateForm() {
@@ -148,7 +156,7 @@ const EmployeeComponent = () => {
                                 {errors.email && <div className='invalid-feedback'>{errors.email} </div>}
                             </div>
 
-                            <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                            <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Submit</button>
 
 
 
