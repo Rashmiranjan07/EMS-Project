f5670d3e454894d4ca0d49dbdb4dda033ca1eb8d
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-19 21:01:38 +0530

    frontend

--- frontend/src/components/ListEmployeeComponent.jsx
+++ frontend/src/components/ListEmployeeComponent.jsx
@@ -54,7 +54,7 @@ const ListEmployeeComponent = () => {
                             <td>{employee.email}</td>
                             <td>
                                 <button className='btn btn-info' onClick={()=>updateEmployee(employee.id)}>Update</button>
-                                <button className=''>Delete</button>
+                                <button className='btn btn-danger'>Delete</button>
                             </td>
                         </tr>
                     ))}
